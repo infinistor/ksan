b6bf3a585c032cf6d27c05a24a205bcd05b08efe
legesse24 <legessez@pspace.co.kr>
2022-05-31 16:25:48 +0900

    index added to each bucket table on objkey

--- core/src/com/pspace/ifs/ksan/objmanager/MongoDataRepository.java
+++ core/src/com/pspace/ifs/ksan/objmanager/MongoDataRepository.java
@@ -424,6 +424,7 @@ public class MongoDataRepository implements DataRepository{
             index.append(VERSIONID, 1);
             index.append(LASTVERSION, 1);
             index.append(DELETEMARKER, 1);
+            index.append(OBJKEY, 1);
             database.getCollection(bt.getName()).createIndex(index, new IndexOptions().unique(true)); 
             //bt = new Bucket(bucketName, bucketName, diskPoolId);
             getUserDiskPool(bt);
