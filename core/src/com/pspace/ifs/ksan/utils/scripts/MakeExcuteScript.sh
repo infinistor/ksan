d4ec8615756206c54e0f7b79dc285fa7b6ac54da
legesse24 <legessez@pspace.co.kr>
2022-06-15 19:24:42 +0900

    utility jar file name chnage and create excute script

---
+++ core/src/com/pspace/ifs/ksan/utils/scripts/MakeExcuteScript.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+PARENTPATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+make_excuteable(){
+Fname="$1"
+
+FULLPATH="$PARENTPATH/../target/$Fname"
+echo "#! /bin/bash" >$FULLPATH
+echo "" >> $FULLPATH
+echo "parent_path=\$( cd \"\$(dirname \"\$\{BASH_SOURCE\[0\]\}\")\" ; pwd -P )" >> $FULLPATH
+echo "java -jar \$parent_path/target/$Fname.jar \"\$\@\"" >> $FULLPATH
+echo "" >> $FULLPATH
+
+chmod +x "$FULLPATH"
+}
+
+make_excuteable "ksanGetAttr"
+make_excuteable "ksanRecovery"
+make_excuteable "ksanFsck"
+make_excuteable "ksancbalance"
+
+exit 0
