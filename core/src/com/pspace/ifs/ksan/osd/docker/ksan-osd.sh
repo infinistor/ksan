75902e760e9bd2e15d0b4928048e45ca3e36b9ab
jwkwak <jwkwak@pspace.co.kr>
2022-08-23 11:53:13 +0900

    명칭변경

--- core/src/com/pspace/ifs/ksan/osd/docker/ksan-osd.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -jar -Dlogback.configurationFile=/usr/local/ksan/etc/ksan-osd-log.xml /app/ksan-osd.jar
---
+++ core/src/com/pspace/ifs/ksan/osd/docker/ksanOSD.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar -Dlogback.configurationFile=/usr/local/ksan/etc/ksanOSD_log.conf /app/ksanOSD.jar
