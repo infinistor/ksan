c53eb91638befb39b8f2793a8757e5e85dbd2d9b
jwkwak <jwkwak@pspace.co.kr>
2022-08-26 15:06:05 +0900

    docker app 실행 경로 변경

--- core/src/com/pspace/ifs/ksan/gw/docker/ksanGW.sh
+++ core/src/com/pspace/ifs/ksan/gw/docker/ksanGW.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar -Dlogback.configurationFile=/usr/local/ksan/etc/ksanGW_log_conf.xml /app/ksanGW.jar
+java -jar -Dlogback.configurationFile=/usr/local/ksan/etc/ksanGW_log_conf.xml /usr/local/ksan/sbin/ksanGW.jar
