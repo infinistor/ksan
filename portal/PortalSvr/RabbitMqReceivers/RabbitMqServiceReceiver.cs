ff9362f04f1bbee611a915543ceddaa65ac6c15d
jwkwak <jwkwak@pspace.co.kr>
2022-10-07 11:22:14 +0900

    fix #112 rabbitmq가 연결될때까지 연결 시도를 계속하도록 변경

--- portal/PortalSvr/RabbitMqReceivers/RabbitMqServiceReceiver.cs
+++ portal/PortalSvr/RabbitMqReceivers/RabbitMqServiceReceiver.cs
@@ -86,10 +86,10 @@ namespace PortalSvr.RabbitMQReceivers
 						return new ResponseMqData(EnumResponseResult.Error, Resource.EC_COMMON__CANNOT_CREATE_INSTANCE, Resource.EM_COMMON__CANNOT_CREATE_INSTANCE);
 
 					// 내부 시스템 API 키 정보를 가져온다.
-					var ResponseApiKey = await ApiKeyProvider.GetMainApiKey();
+					var ApiKey = await ApiKeyProvider.GetMainApiKey();
 
 					// API 키를 가져오는데 실패한 경우
-					if (ResponseApiKey == null)
+					if (ApiKey == null)
 						return new ResponseMqData(EnumResponseResult.Error, Resource.EC_COMMON__NOT_FOUND, Resource.EM_COMMON__NOT_FOUND);
 
 					// 서비스 상태 관련인 경우
@@ -104,7 +104,7 @@ namespace PortalSvr.RabbitMQReceivers
 						var Request = JsonConvert.DeserializeObject<RequestServiceState>(json);
 
 						// 서버 상태 수정
-						var Response = await DataProvider.UpdateState(Request, ResponseApiKey.UserId, ResponseApiKey.UserName);
+						var Response = await DataProvider.UpdateState(Request, ApiKey.UserId, ApiKey.UserName);
 
 						Result.CopyValueFrom(Response);
 						Result.IsProcessed = true;
@@ -121,7 +121,7 @@ namespace PortalSvr.RabbitMQReceivers
 						var Request = JsonConvert.DeserializeObject<RequestServiceHaAction>(json);
 
 						// 서비스 HA 상태 수정
-						var Response = await DataProvider.UpdateHaAction(Request, ResponseApiKey.UserId, ResponseApiKey.UserName);
+						var Response = await DataProvider.UpdateHaAction(Request, ApiKey.UserId, ApiKey.UserName);
 
 						Result.CopyValueFrom(Response);
 						Result.IsProcessed = true;
