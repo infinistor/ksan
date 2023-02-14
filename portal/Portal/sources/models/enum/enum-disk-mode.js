833a979dcb0c93c2f8864aa28a4f2e0552fd4173
jwkwak <jwkwak@pspace.co.kr>
2023-02-14 18:50:16 +0900

    #149 디스크 Maintenance 모드 추가

--- portal/Portal/sources/models/enum/enum-disk-mode.js
+++ portal/Portal/sources/models/enum/enum-disk-mode.js
@@ -1,8 +1,17 @@
 const DiskMode = [
 	{Id:"ReadOnly", value:"Read Only"},
-	{Id:"ReadWrite", value:"Read/Write"}
+	{Id:"ReadWrite", value:"Read/Write"},
+	{Id:"Maintenance", value:"Maintenance"}
 ];
 
 export function getDiskMode() {
 	return DiskMode;
+}
+export function getDiskModeToValue(value)
+{
+	switch (value){
+		case "Read Only": return "ReadOnly";
+		case "Read/Write": return "ReadWrite";
+		case "Maintenance": return "Maintenance";
+	}
 }
