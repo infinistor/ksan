6d388bb16355ae8ca77ff7d9c5ec42c3a8c05620
jwkwak <jwkwak@pspace.co.kr>
2023-03-30 10:56:11 +0900

    #150 User Storage Class 관리 API 추가

---
+++ portal/Portal/sources/models/load/loadUserStorages.js
@@ -0,0 +1,36 @@
+/*
+* Copyright (c) 2021 PSPACE, inc. KSAN Development Team ksan@pspace.co.kr
+* KSAN is a suite of free software: you can redistribute it and/or modify it under the terms of
+* the GNU General Public License as published by the Free Software Foundation, either version
+* 3 of the License.See LICENSE for details
+*
+* 본 프로그램 및 관련 소스코드, 문서 등 모든 자료는 있는 그대로 제공이 됩니다.
+* KSAN 프로젝트의 개발자 및 개발사는 이 프로그램을 사용한 결과에 따른 어떠한 책임도 지지 않습니다.
+* KSAN 개발팀은 사전 공지, 허락, 동의 없이 KSAN 개발에 관련된 모든 결과물에 대한 LICENSE 방식을 변경 할 권리가 있습니다.
+*/
+
+/**
+ * 서버 목록을 가져온다.
+ * @returns Servers. json:{id:"Server Id", value:"Server Name"}
+ */
+export function loadUserStorages(userId) {
+	return webix
+		.ajax()
+		.get(`/api/v1/KsanUsers/StorageClass/${userId}`)
+		.then(
+			function (data) {
+				var response = data.json();
+				if (response.Result == "Error") {
+					webix.message({ text: response.Message, type: "error", expire: 5000 });
+					return null;
+				} else {
+					return response.Data.Items;
+				}
+			},
+			function (error) {
+				var response = JSON.parse(error.response);
+				webix.message({ text: response.Message, type: "error", expire: 5000 });
+				return null;
+			}
+		);
+}
