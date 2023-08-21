6da2432e3f11c728eeaa3090c5997c3eda6931e4
HeoKyungseok <87405823+HeoKyungseok@users.noreply.github.com>
2023-08-21 19:17:53 +0900

    I18N - DM_DEFAULT_ENCODING fix (#191)

--- core/src/com/pspace/ifs/ksan/gw/api/PostObject.java
+++ core/src/com/pspace/ifs/ksan/gw/api/PostObject.java
@@ -12,6 +12,7 @@ package com.pspace.ifs.ksan.gw.api;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
@@ -44,6 +45,7 @@ import com.pspace.ifs.ksan.gw.sign.S3Signing;
 import com.pspace.ifs.ksan.gw.utils.GWConfig;
 import com.pspace.ifs.ksan.gw.utils.GWConstants;
 import com.pspace.ifs.ksan.gw.utils.GWUtils;
+import com.pspace.ifs.ksan.libs.Constants;
 import com.pspace.ifs.ksan.libs.PrintStack;
 import com.pspace.ifs.ksan.objmanager.Metadata;
 
@@ -81,7 +83,7 @@ public class PostObject extends S3Request {
         if (!Strings.isNullOrEmpty(dataPostObject.getPolicy())) {
 			Decoder decoder = Base64.getDecoder();
 			byte[] bytePostPolicy = decoder.decode(dataPostObject.getPolicy());
-			String postPolicy = new String(bytePostPolicy);
+			String postPolicy = new String(bytePostPolicy, Charset.forName(Constants.UTF_8));
 			ObjectMapper jsonMapper = new ObjectMapper();
 
 			PostPolicy postPolicyJson = null;
