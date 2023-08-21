5c50f2d8fca6216ed7abbaaa7aae26db4e36e9af
HeoKyungseok <87405823+HeoKyungseok@users.noreply.github.com>
2023-08-21 18:41:32 +0900

    I18N - DM_DEFAULT_ENCODING fix (#191)

--- core/src/com/pspace/ifs/ksan/gw/api/GetBucketLogging.java
+++ core/src/com/pspace/ifs/ksan/gw/api/GetBucketLogging.java
@@ -11,6 +11,7 @@
 package com.pspace.ifs.ksan.gw.api;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 import jakarta.servlet.http.HttpServletResponse;
 import com.google.common.base.Strings;
@@ -18,6 +19,7 @@ import com.pspace.ifs.ksan.gw.exception.GWErrorCode;
 import com.pspace.ifs.ksan.gw.exception.GWException;
 import com.pspace.ifs.ksan.gw.identity.S3Bucket;
 import com.pspace.ifs.ksan.gw.identity.S3Parameter;
+import com.pspace.ifs.ksan.libs.Constants;
 import com.pspace.ifs.ksan.libs.PrintStack;
 import com.pspace.ifs.ksan.gw.utils.GWConstants;
 import com.pspace.ifs.ksan.gw.utils.GWUtils;
@@ -53,10 +55,10 @@ public class GetBucketLogging extends S3Request {
 		try {
 			if (!Strings.isNullOrEmpty(logging)) {
 				s3Parameter.getResponse().setContentType(GWConstants.XML_CONTENT_TYPE);
-				s3Parameter.getResponse().getOutputStream().write(logging.getBytes());
+				s3Parameter.getResponse().getOutputStream().write(logging.getBytes(Charset.forName(Constants.UTF_8)));
 			} else {
                 s3Parameter.getResponse().setContentType(GWConstants.XML_CONTENT_TYPE);
-				s3Parameter.getResponse().getOutputStream().write(GWConstants.LOG_GET_BUCKET_LOGGING_EMPTY.getBytes());
+				s3Parameter.getResponse().getOutputStream().write(GWConstants.LOG_GET_BUCKET_LOGGING_EMPTY.getBytes(Charset.forName(Constants.UTF_8)));
             }
 		} catch (IOException e) {
 			PrintStack.logging(logger, e);
