c973efefc732d992ddfbccb212ebf0a5f617728e
HeoKyungseok <87405823+HeoKyungseok@users.noreply.github.com>
2023-08-23 12:48:32 +0900

    STYLE-DLS_DEAD_LOCAL_STORE (#191)

--- core/src/com/pspace/ifs/ksan/gw/api/CompleteMultipartUpload.java
+++ core/src/com/pspace/ifs/ksan/gw/api/CompleteMultipartUpload.java
@@ -85,7 +85,8 @@ public class CompleteMultipartUpload extends S3Request {
 
 		String multipartXml = s3RequestData.getMultipartXml();
 		XmlMapper xmlMapper = new XmlMapper();
-		CompleteMultipartUploadRequest completeMultipartUpload = new CompleteMultipartUploadRequest();
+
+		CompleteMultipartUploadRequest completeMultipartUpload = null;
 		try {
 			completeMultipartUpload = xmlMapper.readValue(multipartXml, CompleteMultipartUploadRequest.class);
 		} catch (JsonMappingException e) {
