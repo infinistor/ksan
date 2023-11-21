51ff79d005ff0d13e9876bacf9fff2e467c64960
HeoKyungseok <heoks@pspace.co.kr>
2023-11-21 16:26:10 +0900

    bucket inventory 관련 추가(#220)

---
+++ core/src/com/pspace/ifs/ksan/gw/api/DeleteBucketInventory.java
@@ -0,0 +1,103 @@
+/*
+* Copyright (c) 2021 PSPACE, inc. KSAN Development Team ksan@pspace.co.kr
+* KSAN is a suite of free software: you can redistribute it and/or modify it under the terms of
+* the GNU General Public License as published by the Free Software Foundation, either version 
+* 3 of the License.  See LICENSE for details
+*
+* 본 프로그램 및 관련 소스코드, 문서 등 모든 자료는 있는 그대로 제공이 됩니다.
+* KSAN 프로젝트의 개발자 및 개발사는 이 프로그램을 사용한 결과에 따른 어떠한 책임도 지지 않습니다.
+* KSAN 개발팀은 사전 공지, 허락, 동의 없이 KSAN 개발에 관련된 모든 결과물에 대한 LICENSE 방식을 변경 할 권리가 있습니다.
+*/
+package com.pspace.ifs.ksan.gw.api;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.pspace.ifs.ksan.gw.exception.GWErrorCode;
+import com.pspace.ifs.ksan.gw.exception.GWException;
+import com.pspace.ifs.ksan.gw.format.AnalyticsConfiguration;
+import com.pspace.ifs.ksan.gw.format.Inventory;
+import com.pspace.ifs.ksan.gw.identity.S3Bucket;
+import com.pspace.ifs.ksan.gw.identity.S3Parameter;
+import com.pspace.ifs.ksan.gw.utils.GWConstants;
+import com.pspace.ifs.ksan.gw.utils.GWUtils;
+import com.pspace.ifs.ksan.libs.PrintStack;
+
+import com.google.common.base.Strings;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.slf4j.LoggerFactory;
+
+public class DeleteBucketInventory extends S3Request {
+
+    public DeleteBucketInventory(S3Parameter s3Parameter) {
+        super(s3Parameter);
+        logger = LoggerFactory.getLogger(DeleteBucketInventory.class);
+    }
+
+    @Override
+    public void process() throws GWException {
+        logger.info(GWConstants.LOG_DELETE_BUCKET_INVENTORY_START);
+		
+		String bucket = s3Parameter.getBucketName();
+		initBucketInfo(bucket);
+        
+		GWUtils.checkCors(s3Parameter);
+
+		if (s3Parameter.isPublicAccess() && GWUtils.isIgnorePublicAcls(s3Parameter)) {
+			throw new GWException(GWErrorCode.ACCESS_DENIED, s3Parameter);
+		}
+		
+        String id = s3RequestData.getId();
+        String inventoryXmls = getBucketInfo().getInventory();
+        if (Strings.isNullOrEmpty(inventoryXmls)) {
+            throw new GWException(GWErrorCode.NO_SUCH_CONFIGURATION, s3Parameter);
+        }
+
+		logger.debug(GWConstants.LOG_GET_BUCKET_INVENTORY, bucket, inventoryXmls);
+
+        String[] inventoryIds = inventoryXmls.split("\\n");
+        XmlMapper xmlMapper = new XmlMapper();
+        String newInventory = "";
+        int count = 0;
+        boolean bFound = false;
+        for (String inventoryId : inventoryIds) {
+            Inventory inventory = null;
+            try {
+                inventory = xmlMapper.readValue(inventoryId, Inventory.class);
+                if (id.equals(inventory.id)) {
+                    bFound = true;
+                    continue;
+                } else {
+                    if (count == 0) {
+                        newInventory = inventoryId;
+                    } else {
+                        newInventory += "\n" + inventoryId;
+                    }
+                    count++;
+                }
+            } catch (JsonMappingException e) {
+                PrintStack.logging(logger, e);
+                throw new GWException(GWErrorCode.INTERNAL_SERVER_ERROR, s3Parameter);
+            } catch (JsonProcessingException e) {
+                PrintStack.logging(logger, e);
+                throw new GWException(GWErrorCode.INTERNAL_SERVER_ERROR, s3Parameter);
+            } catch (IOException e) {
+                PrintStack.logging(logger, e);
+                throw new GWException(GWErrorCode.INTERNAL_SERVER_ERROR, s3Parameter);
+            }
+        }
+
+		updateBucketInventory(bucket, newInventory);
+
+        if (bFound) {
+            s3Parameter.getResponse().setStatus(HttpServletResponse.SC_NO_CONTENT);
+        } else {
+            throw new GWException(GWErrorCode.NO_SUCH_CONFIGURATION, s3Parameter);
+        }
+    }
+    
+}
--- core/src/com/pspace/ifs/ksan/gw/api/S3RequestFactory.java
+++ core/src/com/pspace/ifs/ksan/gw/api/S3RequestFactory.java
@@ -40,6 +40,7 @@ public class S3RequestFactory {
 	private final String OP_DELETE_REPLICATION = "REST.DELETE.REPLICATION";
 	private final String OP_DELETE_BUCKET_TAG_INDEX = "REST.DELETE.BUCKET.TAG_INDEX";
 	private final String OP_DELETE_BUCKET_ANALYTICS = "REST.DELETE.BUCKET.ANALYTICS";
+	private final String OP_DELETE_BUCKET_INVENTORY = "REST.DELETE.BUCKET.INVENTORY";
 
 	private final String OP_GET_LISTBUCKET = "REST.GET.LISTBUCKET";
 	private final String OP_GET_WEBSITE = "REST.GET.WEBSITE";
@@ -59,6 +60,8 @@ public class S3RequestFactory {
 	private final String OP_GET_BUCKET_PAYMENT = "REST.GET.BUCKET.PAYMENT";
 	private final String OP_GET_BUCKET_NOTIFICATION = "REST.GET.BUCKET.NOTIFICATION";
 	private final String OP_GET_LISTBUCKETANALYTICS = "REST.GET.LISTBUCKETANALYTICS";
+	private final String OP_GET_BUCKET_INVENTORY = "REST.GET.BUCKET.INVENTORY";
+	private final String OP_GET_LISTBUCKETINVENTORY = "REST.GET.LISTBUCKETINVENTORY";
 
 	private final String OP_GET_ENCRYPTION = "REST.GET.ENCRYPTION";
 	private final String OP_GET_BUCKET_ACL = "REST.GET.BUCKET.ACL";
@@ -103,7 +106,8 @@ public class S3RequestFactory {
 	private final String OP_PUT_BUCKET_ACCELERATE = "REST.PUT.BUCKET.ACCELERATE";
 	private final String OP_PUT_BUCKET_ANALYTICS = "REST.PUT.BUCKET.ANALYTICS";
 	private final String OP_PUT_BUCKET_PAYMENT = "REST.PUT.BUCKET.PAYMENT";
-	private final String OP_PUT_BUCKET_NOTIFICATION = "REST.PUT_BUCKET.NOTIFICATION";
+	private final String OP_PUT_BUCKET_NOTIFICATION = "REST.PUT.BUCKET.NOTIFICATION";
+	private final String OP_PUT_BUCKET_INVENTORY = "REST.PUT.BUCKET.INVENTORY";
 	
 	private final String OP_PUT_OBJECT_PART_COPY = "REST.PUT.OBJECT.PART.COPY";
 	private final String OP_PUT_OBJECT_PART = "REST.PUT.OBJECT.PART";
@@ -178,6 +182,9 @@ public class S3RequestFactory {
 					} else if (GWConstants.EMPTY_STRING.equals(s3Parameter.getRequest().getParameter(GWConstants.PARAMETER_ANALYTICS))) {
 						s3Parameter.setOperation(OP_DELETE_BUCKET_ANALYTICS);
 						return new DeleteBucketAnalytics(s3Parameter);
+					} else if (GWConstants.EMPTY_STRING.equals(s3Parameter.getRequest().getParameter(GWConstants.PARAMETER_INVENTORY))) {
+						s3Parameter.setOperation(OP_DELETE_BUCKET_INVENTORY);
+						return new DeleteBucketInventory(s3Parameter);
 					}
 
 					if (s3Parameter.isPublicAccess()) {
@@ -300,6 +307,15 @@ public class S3RequestFactory {
 					} else if (GWConstants.EMPTY_STRING.equals(s3Parameter.getRequest().getParameter(GWConstants.PARAMETER_NOTIFICATION))) {
 						s3Parameter.setOperation(OP_GET_BUCKET_NOTIFICATION);
 						return new GetBucketNotification(s3Parameter);
+					} else if (GWConstants.EMPTY_STRING.equals(s3Parameter.getRequest().getParameter(GWConstants.PARAMETER_INVENTORY))) {
+						String id = s3Parameter.getRequest().getParameter(GWConstants.PARAMETER_ID);
+						if (id != null) {
+							s3Parameter.setOperation(OP_GET_BUCKET_INVENTORY);
+							return new GetBucketInventory(s3Parameter);
+						} else {
+							s3Parameter.setOperation(OP_GET_LISTBUCKETINVENTORY);
+							return new ListBucketInventory(s3Parameter);
+						}
 					}
 
 					if (s3Parameter.getRequest().getParameter(GWConstants.PARAMETER_LIST_TYPE) != null) {
@@ -468,6 +484,9 @@ public class S3RequestFactory {
 					} else if (GWConstants.EMPTY_STRING.equals(s3Parameter.getRequest().getParameter(GWConstants.PARAMETER_NOTIFICATION))) { 
 						s3Parameter.setOperation(OP_PUT_BUCKET_NOTIFICATION);
 						return new PutBucketNotification(s3Parameter);
+					} else if (GWConstants.EMPTY_STRING.equals(s3Parameter.getRequest().getParameter(GWConstants.PARAMETER_INVENTORY))) { 
+						s3Parameter.setOperation(OP_PUT_BUCKET_INVENTORY);
+						return new PutBucketInventory(s3Parameter);
 					}
 
 					if (s3Parameter.isPublicAccess()) {
